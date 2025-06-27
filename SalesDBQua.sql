4bca1b76c85c9662fde2e22e49201895e1b737d5
Md. Al-Amin Kabir <109284049+alamineee18@users.noreply.github.com>
2025-06-27 22:01:05 +0600

    Add advanced SQL queries for Sales and Customers
    Added several SQL queries demonstrating NULL handling, conditional logic with CASE, aggregation by category, employee gender formatting, and customer country code abbreviation. These queries enhance data analysis and reporting capabilities for the Sales and Customers tables.

--- SalesDBQua.sql
+++ SalesDBQua.sql
@@ -38,3 +38,84 @@ SELECT
 	MONTH(CreationTime) Month,
 	DAY(CreationTime) Day
 FROM Sales.Orders
+
+
+--NULL Operation
+SELECT 
+    CustomerID, 
+    FirstName, 
+    LastName, 
+    FirstName + ' ' + COALESCE(LastName, '') AS FullName, 
+    Score,
+	COALESCE(Score, 0) + 10 AS ScoreWithBonus
+FROM 
+    Sales.Customers;
+
+
+SELECT 
+	OrderID,
+	Sales,
+	Quantity,
+	Sales/ NULLIF (Quantity,0) AS price
+FROM 
+	Sales.Orders;
+
+SELECT 
+    OrderID,
+    Sales,
+    CASE
+        WHEN Sales > 50 THEN 'High'
+        WHEN Sales <= 20 THEN 'Medium'
+		ELSE 'LOW'
+    END Category
+FROM 
+    Sales.Orders;
+
+SELECT 
+    Category,
+    SUM(Sales) AS TotalSales
+FROM (
+    SELECT 
+        OrderID,
+        Sales,
+        CASE
+            WHEN Sales > 50 THEN 'High'
+            WHEN Sales > 20 THEN 'Medium'
+            ELSE 'Low'
+        END AS Category
+    FROM 
+        Sales.Orders
+) t
+GROUP BY 
+    Category
+ORDER BY
+	TotalSales DESC;
+
+
+-- Retrieve employee details with gender displayed as full text
+SELECT 
+    EmployeeID,
+    CONCAT(FirstName,' ',LastName) AS Name,
+    --Gender,
+	CASE 
+		WHEN Gender = 'F' THEN 'Female'
+		WHEN Gender = 'M' THEN 'Male'
+		ELSE 'Not Avaiable'
+	END GenderFF
+FROM 
+    Sales.Employees;
+
+-- Retrieve customer details with abbreviated country code
+SELECT 
+    CustomerID,
+    FirstName,
+    LastName,
+    Country,
+    CASE
+        WHEN Country = 'Germany' THEN 'DE'
+        WHEN Country = 'USA' THEN 'US'
+        ELSE 'n/a'
+    END AS CountryCode
+FROM 
+    Sales.Customers;
+
